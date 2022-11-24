1de7eeeb2f9517f9c7b2e257f2a412168c175306
Hakan Dındış <hakandindis@gmail.com>
2022-11-24 14:03:50 +0300

    characters endpoint added to API

---
+++ QuizAPI/Character.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizAPI
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public string Film { get; set; }
+
+    }
+}
---
+++ QuizAPI/Controllers/CharacterController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuizAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static readonly string[] Names = new[]
+        {
+        "Hakan", "Kaan", "Can", "Refik", "Ulaş" };
+
+            private static readonly string[] Films = new[]
+        {
+        "Star Wars", "Lotr", "Matrix", "Lucy", "Mild"
+    };
+
+        private readonly ILogger<CharacterController> _logger;
+
+        public CharacterController(ILogger<CharacterController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "character")]
+        public IEnumerable<Character> Get()
+        {
+            return Enumerable.Range(1, 4).Select(index => new Character
+            {
+                Id = index,
+                Name = Names[index],
+                Film = Films[index]
+            })
+            .ToArray();
+        }
+    }
+}
